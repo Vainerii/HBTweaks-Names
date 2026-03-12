372623c97d9213b0105f8d4071f8eb5fa06ef797
vainerii <vainerii@nomail.fr>
2026-03-12 03:05:33 +0100

    Changed the way debug works, added fabric and mc versions

--- src/main/java/vai/hbtweaks/names/HBTweakNames.java
+++ src/main/java/vai/hbtweaks/names/HBTweakNames.java
@@ -5,21 +5,20 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+
 public class HBTweakNames implements ModInitializer {
-	public static final String MOD_ID = "herobrine-companion";
+	public static final String MOD_ID = "hbtweaks-names";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	public static final boolean DEBUG_MODE = false;
+	public static final boolean DEBUG_MODE = new File(".hbtweaks_debug").exists();
 
-	private static HBTweakNames instance;
-
-	public static HBTweakNames getInstance() {
-		return instance;
-	}
+	public static HBTweakNames instance;
 
 	@Override
 	public void onInitialize() {
 		HBTweakNames.instance = this;
-	}
+		LOGGER.info("Initialisation de " + MOD_ID);
+    }
 }
